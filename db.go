7f48186e06a6c856fcb7111f42fa251c44292838
Tuxer <e.yulian@gmail.com>
2018-08-24 14:40:55 +0700

    handle longtext

--- db.go
+++ db.go
@@ -2,29 +2,29 @@
 * Created by Visual Studio Code.
 * User: tuxer
 * Created At: 2017-12-16 16:36:19
-*/
+ */
 
 package db
 
 import (
-	"regexp"
 	"database/sql"
+	"regexp"
 )
 
 var (
-	defaultCn			*Connection
-	regexStringColType	*regexp.Regexp
+	defaultCn          *Connection
+	regexStringColType *regexp.Regexp
 )
 
 //NewConnection ...
-func NewConnection(host string, username string, password string, databaseName string) (*Connection, error)	{
-	db, e := sql.Open(`mysql`, username + `:` + password + `@tcp(` + host + `)/` + 
-		databaseName + `?parseTime=true&loc=Local&collation=latin1_general_ci`)
+func NewConnection(host string, username string, password string, databaseName string) (*Connection, error) {
+	db, e := sql.Open(`mysql`, username+`:`+password+`@tcp(`+host+`)/`+
+		databaseName+`?parseTime=true&loc=Local&collation=latin1_general_ci`)
 
-	if e != nil	{
+	if e != nil {
 		return nil, e
 	}
-	if e := db.Ping(); e != nil	{
+	if e := db.Ping(); e != nil {
 		return nil, e
 	}
 
@@ -32,9 +32,9 @@ func NewConnection(host string, username string, password string, databaseName s
 	return defaultCn, nil
 }
 
-func getRegex() *regexp.Regexp	{
-	if regexStringColType == nil	{
-		regexStringColType, _ = regexp.Compile(`(?i)^.*CHAR|TEXT$`)
+func getRegex() *regexp.Regexp {
+	if regexStringColType == nil {
+		regexStringColType, _ = regexp.Compile(`(?i)^.*CHAR|.*TEXT$`)
 	}
 	return regexStringColType
 }
