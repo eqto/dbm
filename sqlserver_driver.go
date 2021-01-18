d781978e253c1356d6253c1433d8565a8fb2f863
tuxer <e.yulian@gmail.com>
2021-01-18 15:26:05 +0700

    improve memory usage

--- sqlserver_driver.go
+++ sqlserver_driver.go
@@ -1,8 +1,10 @@
 package db
 
 import (
+	"database/sql"
 	"fmt"
 	"net/url"
+	"reflect"
 	"regexp"
 	"strings"
 )
@@ -48,4 +50,48 @@ func (s *sqlserverDriver) insertReturnID(tx *Tx, tableName string, fields []stri
 		return 0, e
 	}
 	return rs.Int(`ID`), nil
+}
+
+func (s *sqlserverDriver) buildContents(colTypes []*sql.ColumnType) ([]interface{}, error) {
+	vals := make([]interface{}, len(colTypes))
+	for idx, colType := range colTypes {
+		scanType := colType.ScanType()
+		switch scanType.Kind() {
+		case reflect.Int8:
+			vals[idx] = new(int8)
+		case reflect.Uint8:
+			vals[idx] = new(uint8)
+		case reflect.Int16:
+			vals[idx] = new(int16)
+		case reflect.Uint16:
+			vals[idx] = new(uint16)
+		case reflect.Int32:
+			vals[idx] = new(int32)
+		case reflect.Uint32:
+			vals[idx] = new(uint32)
+		case reflect.Int64:
+			vals[idx] = new(int64)
+		case reflect.Uint64:
+			vals[idx] = new(uint64)
+		case reflect.Float32:
+			vals[idx] = new(float32)
+		case reflect.Float64:
+			vals[idx] = new(float64)
+		case reflect.Slice:
+			vals[idx] = new([]byte)
+		case reflect.Struct:
+			switch scanType.Name() {
+			case `NullInt64`:
+				vals[idx] = new(sql.NullInt64)
+			case `NullFloat64`:
+				vals[idx] = new(sql.NullFloat64)
+			case `NullTime`:
+				vals[idx] = new(sql.NullTime)
+			}
+		}
+		if vals[idx] == nil {
+			return nil, fmt.Errorf(`not supported type %s:%s as kind %s`, colType.Name(), colType.DatabaseTypeName(), scanType.Kind().String())
+		}
+	}
+	return vals, nil
 }
