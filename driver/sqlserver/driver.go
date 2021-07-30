046c1f78ca0ba903a76baaf9dc06676dd569bd92
tuxer <e.yulian@gmail.com>
2021-07-30 15:42:35 +0700

    select statement sqlserver

--- driver/sqlserver/driver.go
+++ driver/sqlserver/driver.go
@@ -1,84 +1,104 @@
 package sqlserver
 
 import (
+	"database/sql"
+	"fmt"
+	"reflect"
+	"regexp"
+
 	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/eqto/go-db"
+	"github.com/eqto/go-db/query"
 )
 
-// func init() {
-// 	db.Register(`sqlserver`, &driver{})
-// }
+func init() {
+	db.Register(`sqlserver`, &Driver{})
+}
 
-// type driver struct {
-// 	db.Driver
-// }
+type Driver struct {
+	db.Driver
+}
 
-// func (*driver) Name() string {
-// 	return `sqlserver`
-// }
+func (Driver) Name() string {
+	return `sqlserver`
+}
 
-// func (*driver) DataSourceName(hostname string, port int, username, password, name string) string {
-// 	return fmt.Sprintf(`%s:%s@tcp(%s:%d)/%s?parseTime=true&loc=Local`,
-// 		username, password,
-// 		hostname, port,
-// 		name,
-// 	)
-// }
-// func (*driver) IsDuplicate(msg string) bool {
-// 	return regexp.MustCompile(`^Duplicate entry.*`).MatchString(msg)
-// }
+func (Driver) Query(stmt interface{}) string {
+	stmt = query.StatementOf(stmt)
+	switch stmt := stmt.(type) {
+	case *query.SelectStmt:
+		return querySelect(stmt)
+	case *query.InsertStmt:
+		return queryInsert(stmt)
+	case *query.UpdateStmt:
+		return queryUpdate(stmt)
+	}
+	return ``
+}
 
-// func (*driver) BuildContents(colTypes []*sql.ColumnType) ([]interface{}, error) {
-// 	vals := make([]interface{}, len(colTypes))
-// 	for idx, colType := range colTypes {
-// 		scanType := colType.ScanType()
-// 		switch scanType.Kind() {
-// 		case reflect.Int8:
-// 			vals[idx] = new(int8)
-// 		case reflect.Uint8:
-// 			vals[idx] = new(uint8)
-// 		case reflect.Int16:
-// 			vals[idx] = new(int16)
-// 		case reflect.Uint16:
-// 			vals[idx] = new(uint16)
-// 		case reflect.Int32:
-// 			vals[idx] = new(int32)
-// 		case reflect.Uint32:
-// 			vals[idx] = new(uint32)
-// 		case reflect.Int64:
-// 			vals[idx] = new(int64)
-// 		case reflect.Uint64:
-// 			vals[idx] = new(uint64)
-// 		case reflect.Float32:
-// 			vals[idx] = new(float32)
-// 		case reflect.Float64:
-// 			vals[idx] = new(float64)
-// 		case reflect.Slice:
-// 			switch colType.DatabaseTypeName() {
-// 			case `DECIMAL`:
-// 				if null, ok := colType.Nullable(); null || ok {
-// 					vals[idx] = new(sql.NullFloat64)
-// 				} else {
-// 					vals[idx] = new(float64)
-// 				}
-// 			default:
-// 				vals[idx] = new([]byte)
-// 			}
-// 		case reflect.Struct:
-// 			switch scanType.Name() {
-// 			case `NullInt64`:
-// 				vals[idx] = new(sql.NullInt64)
-// 			case `NullFloat64`:
-// 				vals[idx] = new(sql.NullFloat64)
-// 			case `NullTime`:
-// 				vals[idx] = new(sql.NullTime)
-// 			}
-// 		}
-// 		if vals[idx] == nil {
-// 			return nil, fmt.Errorf(`not supported type %s:%s as kind %s`, colType.Name(), colType.DatabaseTypeName(), scanType.Kind().String())
-// 		}
-// 	}
-// 	return vals, nil
-// }
+func (Driver) DataSourceName(hostname string, port int, username, password, name string) string {
+	return fmt.Sprintf(`%s:%s@tcp(%s:%d)/%s?parseTime=true&loc=Local`,
+		username, password,
+		hostname, port,
+		name,
+	)
+}
+func (Driver) IsDuplicate(msg string) bool {
+	return regexp.MustCompile(`^Duplicate entry.*`).MatchString(msg)
+}
+
+func (Driver) BuildContents(colTypes []*sql.ColumnType) ([]interface{}, error) {
+	vals := make([]interface{}, len(colTypes))
+	for idx, colType := range colTypes {
+		scanType := colType.ScanType()
+		switch scanType.Kind() {
+		case reflect.Int8:
+			vals[idx] = new(int8)
+		case reflect.Uint8:
+			vals[idx] = new(uint8)
+		case reflect.Int16:
+			vals[idx] = new(int16)
+		case reflect.Uint16:
+			vals[idx] = new(uint16)
+		case reflect.Int32:
+			vals[idx] = new(int32)
+		case reflect.Uint32:
+			vals[idx] = new(uint32)
+		case reflect.Int64:
+			vals[idx] = new(int64)
+		case reflect.Uint64:
+			vals[idx] = new(uint64)
+		case reflect.Float32:
+			vals[idx] = new(float32)
+		case reflect.Float64:
+			vals[idx] = new(float64)
+		case reflect.Slice:
+			switch colType.DatabaseTypeName() {
+			case `DECIMAL`:
+				if null, ok := colType.Nullable(); null || ok {
+					vals[idx] = new(sql.NullFloat64)
+				} else {
+					vals[idx] = new(float64)
+				}
+			default:
+				vals[idx] = new([]byte)
+			}
+		case reflect.Struct:
+			switch scanType.Name() {
+			case `NullInt64`:
+				vals[idx] = new(sql.NullInt64)
+			case `NullFloat64`:
+				vals[idx] = new(sql.NullFloat64)
+			case `NullTime`:
+				vals[idx] = new(sql.NullTime)
+			}
+		}
+		if vals[idx] == nil {
+			return nil, fmt.Errorf(`not supported type %s:%s as kind %s`, colType.Name(), colType.DatabaseTypeName(), scanType.Kind().String())
+		}
+	}
+	return vals, nil
+}
 
 // func (*driver) BuildQuery(param db.QueryParameter) string {
 // 	s := strings.Builder{}
---
+++ driver/sqlserver/select.go
@@ -0,0 +1,58 @@
+package sqlserver
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/eqto/go-db/query"
+)
+
+func querySelect(stmt *query.SelectStmt) string {
+	sql := strings.Builder{}
+
+	fields := []string{}
+	tables := []string{}
+
+	tableStmt := query.TableStmtOf(stmt)
+	for {
+		for _, field := range query.FieldsOf(stmt) {
+			if field.Alias != `` {
+				fields = append(fields, fmt.Sprintf(`%s AS %s`, field.Name, field.Alias))
+			} else {
+				fields = append(fields, field.Name)
+			}
+		}
+		table := query.TableOf(tableStmt)
+		tableStr := table.Name
+		if table.Alias != `` {
+			tableStr += ` ` + table.Alias
+		}
+		if len(tables) == 0 {
+			tables = append(tables, tableStr)
+		} else {
+			tables = append(tables, fmt.Sprintf(`INNER JOIN %s ON %s`, tableStr, table.Condition))
+		}
+
+		tableStmt = query.JoinOf(tableStmt)
+		if tableStmt == nil {
+			break
+		}
+	}
+	strFields := strings.Join(fields, `, `)
+	if strFields == `` {
+		strFields = `*`
+	}
+	sql.WriteString(fmt.Sprintf(`SELECT %s FROM %s`, strFields, strings.Join(tables, ` `)))
+
+	if wheres := query.WhereOf(stmt); len(wheres) > 0 {
+		sql.WriteString(fmt.Sprintf(` WHERE %s`, strings.Join(wheres, ` `)))
+	}
+	if orderBys := query.OrderByOf(stmt); len(orderBys) > 0 {
+		sql.WriteString(fmt.Sprintf(` ORDER BY %s`, strings.Join(orderBys, `, `)))
+	}
+
+	if offset, count := query.LimitOf(stmt); count > 0 {
+		sql.WriteString(fmt.Sprintf(` OFFSET %d ROWS FETCH NEXT %d ROWS ONLY`, offset, count))
+	}
+	return sql.String()
+}
