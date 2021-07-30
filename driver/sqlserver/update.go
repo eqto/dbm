7ac36dc0d55bd9bb9876f25d8dc680fc7a2633fa
tuxer <e.yulian@gmail.com>
2021-07-30 15:49:03 +0700

    update statement sqlserver

---
+++ driver/sqlserver/update.go
@@ -0,0 +1,23 @@
+package sqlserver
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/eqto/go-db/query"
+)
+
+func queryUpdate(stmt *query.UpdateStmt) string {
+	table := query.TableOf(stmt)
+	fieldStrs := []string{}
+	for _, field := range table.Fields {
+		fieldStrs = append(fieldStrs, field.Name+` = `+field.Placeholder)
+	}
+	sql := strings.Builder{}
+	sql.WriteString(fmt.Sprintf(`UPDATE %s SET %s`, table.Name, strings.Join(fieldStrs, `, `)))
+	if conditions := query.WhereOf(stmt); len(conditions) > 0 {
+		sql.WriteString(fmt.Sprintf(` WHERE %s`, strings.Join(conditions, ` `)))
+	}
+
+	return sql.String()
+}
